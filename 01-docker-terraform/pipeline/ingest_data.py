3f53caf95ffb7f06f4638ef91a1fbc921c31011e
Hamm-code <sentsdrimma123@gmail.com>
2026-01-30 01:29:01 +0200

    added pg admin cli

--- 01-docker-terraform/pipeline/ingest_data.py
+++ 01-docker-terraform/pipeline/ingest_data.py
@@ -37,8 +37,11 @@ parse_dates = [
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default=5432, type=int, help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--target-table', default='yellow_taxi_trips', help='Target table name')
-def run(pg_user, pg_pass, pg_host, pg_port, pg_db, target_table):
+@click.option('--year', default='2021', type=int, help='Year of the Data')
+@click.option('--month', default='1', type=int, help='Month of the Data')
+@click.option('--chunksize', default='100000', type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+def run(pg_user, pg_pass, pg_host, year, month, chunksize, pg_port, pg_db, target_table):
     prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
 
